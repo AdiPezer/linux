5649342b47cd800e7ee89bc5fd875a56969e9750
Adnan Pezer <adnan.pezeer@hotmail.com>
2018-08-18 12:59:58 +0200

    nova komanda

---
+++ ls-a.sh
@@ -0,0 +1,9 @@
+echo "pokrecem ls komandu i prikazujem sadrzaj trenutnog direktorija i prikazujem skrivene direktorije"
+```
+adi_user@emin-TravelMate-5720:~$ ls -a
+.              .bashrc  Documents         .ICEauthority  .profile                   Videos
+..             .cache   Downloads         .local         Public                     .Xauthority
+adi            .config  examples.desktop  .mozilla       .sudo_as_admin_successful  .xsession-errors
+.bash_history  Desktop  .gconf            Music          Templates                  .xsession-errors.old
+.bash_logout   .dmrc    .gnupg            Pictures       test      
+```
