99724841db4def41ff38ada7c3e8041f36907c81
Adnan Pezer <adnan.pezeer@hotmail.com>
2018-08-18 13:12:01 +0200

    nova komanda

---
+++ ls-l.sh
@@ -0,0 +1,16 @@
+echo "pokrecem ls-l komandu i mjenjam format u kome se lista direktorij, i za svaki fajl prikazujem odredjene podatke o njemu"
+```
+ls -l
+total 56
+drwxrwxr-x 4 adi_user adi_user 4096 Aug  6 13:44 adi
+drwxr-xr-x 2 adi_user adi_user 4096 Aug  4 11:36 Desktop
+drwxr-xr-x 2 adi_user adi_user 4096 Aug  4 11:36 Documents
+drwxr-xr-x 4 adi_user adi_user 4096 Aug  6 11:57 Downloads
+-rw-r--r-- 1 adi_user adi_user 8980 Aug  4 11:34 examples.desktop
+drwxr-xr-x 2 adi_user adi_user 4096 Aug  4 11:36 Music
+drwxr-xr-x 2 adi_user adi_user 4096 Aug  4 11:36 Pictures
+drwxr-xr-x 2 adi_user adi_user 4096 Aug  4 11:36 Public
+drwxr-xr-x 2 adi_user adi_user 4096 Aug  4 11:36 Templates
+drwxrwxr-x 2 adi_user adi_user 4096 Aug  4 14:38 test
+drwxr-xr-x 2 adi_user adi_user 4096 Aug  4 11:36 Videos
+```
